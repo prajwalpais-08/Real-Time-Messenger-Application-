0c61246e395a243bc9774b5461cf9f5787a73e19
Prajwal Royston Cordiero <prajwalcordiero@gmail.com>
2026-03-05 20:37:44 +0530

    added separate folder for frontend and server

---
+++ frontend/src/component/SideToolbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SideToolbar.css";
+
+function SideToolbar(){
+  return(
+    <div className="sidebar">
+
+      
+    
+      <button>🎨</button>
+      <button>✏</button>
+      <button>🧽</button>
+      <button>🔤</button>
+      <button>📥</button>
+      <button>↩</button>
+      <button>↪</button>
+      <button>🗑</button>
+
+    </div>
+  )
+}
+
+export default SideToolbar;
