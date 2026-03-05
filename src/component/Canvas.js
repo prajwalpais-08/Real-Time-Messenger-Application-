2740bccb1e6b7ab103f662a237f535a830c926a3
Prajwal Pais <prajwalnoel2005>
2026-03-05 19:36:25 +0530

    Add whiteboard canvas

---
+++ src/component/Canvas.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Canvas.css";
+
+function Canvas(){
+  return(
+    <div className="canvas-area">
+
+      <div className="board">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Canvas;
