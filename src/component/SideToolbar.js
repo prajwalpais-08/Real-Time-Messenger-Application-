3e6e93a770c7eef9ab91b0115f170479ef80a6e5
Prajwal Pais <prajwalnoel2005>
2026-03-05 19:33:53 +0530

    Add sidebar toolbar

---
+++ src/component/SideToolbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SideToolbar.css";
+
+function SideToolbar(){
+  return(
+    <div className="sidebar">
+
+      <button>🖱</button>
+      <button>📄</button>
+      <button>🔳</button>
+      <button>🎨</button>
+      <button>✏</button>
+      <button>🧽</button>
+      <button>🔤</button>
+      <button>🖼</button>
+      <button>📥</button>
+      <button>↩</button>
+      <button>↪</button>
+      <button>🗑</button>
+
+    </div>
+  )
+}
+
+export default SideToolbar;
