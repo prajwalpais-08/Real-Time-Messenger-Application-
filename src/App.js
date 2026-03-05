f9fdf08702f05620355995608b2c7d2cfb8a397c
Prajwal Pais <prajwalnoel2005>
2026-03-05 19:16:15 +0530

    Update main App Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TopBar from "./component/TopBar";
+import SideToolbar from "./component/SideToolbar";
+import Canvas from "./component/Canvas";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <TopBar />
+
+      <div className="workspace">
+
+        <SideToolbar />
+
+        <Canvas />
+
+      </div>
+
     </div>
   );
 }
